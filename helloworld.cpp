f8b6110f01509052d7b155b67119070575515abc
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2023-12-19 16:53:52 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,8 @@
 int main(){
     std::cout << 'Tuesday' << std::endl;
     std::cout << 'Yeah' << std::endl;
-    return 0;
+    bool online = true;
+    double amount = 14.7;
+    string random = "I am here again.";
+    std::cout << string;
 } 

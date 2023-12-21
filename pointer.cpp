c4324a828f683bf9f559ed9025877c5a054c6565
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2023-12-21 14:40:23 -0500

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
+    int *p = arr;
+
+    cout << *p << endl;
+    cout << *(p + 4);
+    cout << 'Something new every day!';
+}

952b373635e04504efb6880f4afa2b4324d9d655
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2023-12-21 13:28:44 -0500

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() 
+{
+    double num1, num2;
+    char operation;
+
+    std::cout << 'first number: ';
+    std::cin >> num1;
+
+    std::cout << 'second number: ';
+    std::cin >> num2;
+
+    std::cout << 'operation of use (+, -, /, *)';
+    std::cin >> operation;
+
+    switch (operation)
+     {
+        case '+': 
+        std::cout << 'result: '  << num1 + num2 << std::endl;
+        break;
+        case '-':
+        std::cout << 'result: ' << num1 - num2 << std::endl;
+        break;
+        case '/':
+        if (num2 != 0) {
+        std::cout << 'result: ' << num1 / num2 << std::endl;
+        } 
+        else {
+            std::cout << 'error: attempted division by zero' << std::endl;
+        }
+        break;
+        case '*':
+        std::cout << 'result:' << num1 * num2 << std::endl;
+        break;
+        default: 
+        std::cout << 'error; invalid operation' << std::endl;
+}
+
+return 0;
+}

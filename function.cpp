81638dd5bef9432353f4769777148472fa5735af
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2023-12-22 09:27:08 -0500

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sum(int x, int y) {
+    std::cout << 'the sum is ' << x + y << '.' << std::endl;
+}
+
+void quotient(int x, int y) {
+    std::cout << 'the quotient is ' << x / y << '.' << std::endl;
+}
+
+int main() {
+    sum(11, 250);
+    quotient(121, 11);
+}

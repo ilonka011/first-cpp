fdb10711aa73a72ddbb38d3d6b11ff5a76a568ab
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2023-12-24 22:16:02 -0500

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void sum(int x, int y, int z, int a) {
+    std::cout << x + y + z + a << std::endl;
+}
+int main() {
+    sum(11, 350, 999, 14);
+    std::cin >> 'What is your name?' >> std::endl;
+    return 0;
+}
